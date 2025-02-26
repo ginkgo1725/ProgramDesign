ab82132ed0a171cf27035f3a52bdf56a0ba578ea
yin <yinx1725@gmail.com>
2025-02-26 15:31:16 +0800

    Polish the interface and fix a few bugs

---
+++ DecompressionTreeBased.h
@@ -0,0 +1,13 @@
+//
+// Created by Ginkgo on 2025/2/22.
+//
+#ifndef DECOMPRESSIONTREEBASED_H
+#define DECOMPRESSIONTREEBASED_H
+#include "Define.h"
+#include "hash.h"
+#include "Tree.h"
+
+
+void DecompressTreeBased();
+
+#endif //DECOMPRESSIONTREEBASED_H
